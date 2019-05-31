5c7d493f1f57980d1776820d28a1afaaf83c5019
Victor <victor.fauchez-le-gouic@outlook.com>
2019-05-31 14:14:54 +0200

    front: basic concept

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function showPreview(input) 
-{
+function showPreview(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) 
@@ -8,8 +7,28 @@ function showPreview(input)
         }
         reader.readAsDataURL(input.files[0]);
     }
+    buttonSwap();
 }
 
+function blank(){
+	document.getElementById("preview").src = "";
+}
 
+function buttonSwap(){
+	let button = document.getElementById("pt_label");
+	if(button.innerHTML == "Analize"){
+		button.innerHTML = "Take another picture";
+		button.setAttribute("for", "pic");
+		button.setAttribute("class", "camera");
+	}
+	else{
+		button.innerHTML = "Analize";
+		button.setAttribute("for", "upload");
+		button.setAttribute("class", "pigeon");
+	}
+}
 
-
+function send(){
+	blank();
+	buttonSwap();
+}
